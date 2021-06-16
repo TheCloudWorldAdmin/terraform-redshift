5930eab9d7b421428f5023e8521dac7c9a8af6e2
wilshan <74950445+wilshan@users.noreply.github.com>
2021-06-16 10:29:29 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
 resource "aws_redshift_cluster" "default" {
-  cluster_identifier = "tf-redshift-cluster"
-  database_name      = "mydb"
-  master_username    = "foo"
-  master_password    = "Mustbe8characters"
-  node_type          = "dc1.large"
-  cluster_type       = var.clusterType>1"multi-node"single-node"
+  cluster_identifier = var.identifier
+  cluster_version = var.version
+  node_type          = var.node_count > 1 ? "dc2.large" : "ra3.4xlarge"
+  number_of_nodes    = var.node_count
+  cluster_type       = var.node_count > 1 ? "multi-node" : "single-node"
+  database_name      = var.db_name
+  master_username    = var.master_user
+  master_password    = var.master_pass
+  allow_version_upgrade = var.version_upgrade
+  
+  
+  
+  
+  port = var.db_port
 }
+
