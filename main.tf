2c2cb7fbf9774d0ffa7b31ef54db7c11c553be61
wilshan <74950445+wilshan@users.noreply.github.com>
2021-06-17 15:37:55 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "aws_redshift_cluster" "redshift_cluster" {
   # Automatic Version & Security group ID details
   
   allow_version_upgrade = var.version_upgrade
-  vpc_security_group_ids = [data.aws_security_group.id]
+  vpc_security_group_ids = [data.aws_security_groups.sg.id]
   
   
   availability_zone = var.az
